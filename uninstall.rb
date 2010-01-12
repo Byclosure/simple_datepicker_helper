55ee169154d8323e9dc99b8dd2b040da4b8dcfc5
Vasco <vasco@byclosure.com>
2010-01-12 17:53:41 +0000

    PLUGIN_ROOT and RAILS_ROOT with defined?

--- uninstall.rb
+++ uninstall.rb
@@ -1,14 +1,15 @@
 # Uninstall hook code here
 
 require 'fileutils'
-PLUGIN_ROOT = File.dirname(__FILE__)
-RAILS_ROOT = File.join(PLUGIN_ROOT, '..','..','..')
+PLUGIN_ROOT = File.dirname(__FILE__) unless defined?(PLUGIN_ROOT)
+RAILS_ROOT = File.join(PLUGIN_ROOT, '..','..','..') unless defined?(RAILS_ROOT)
 
 # Workaround a problem with script/plugin and http-based repos.
 # See http://dev.rubyonrails.org/ticket/8189
 Dir.chdir(Dir.getwd.sub(/vendor.*/, '')) do
 
   FileUtils.rm File.join(RAILS_ROOT,"public","javascripts","simple_datepicker.js")
+  FileUtils.rm File.join(RAILS_ROOT,"public","javascripts","simple_datepicker_runner.js")
   FileUtils.rm File.join(RAILS_ROOT,"public","stylesheets","simple_datepicker.css")
 
 end
