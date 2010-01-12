86b9bd8a1c684a6cbbeb7e5e89d97d55c4bb751d
Vasco <vasco@byclosure.com>
2010-01-12 17:52:31 +0000

    PLUGIN_ROOT and RAILS_ROOT with defined?

--- install.rb
+++ install.rb
@@ -1,7 +1,8 @@
 # Install hook code here
 
 require 'fileutils'
-PLUGIN_ROOT = File.dirname(__FILE__)
+PLUGIN_ROOT = File.dirname(__FILE__) unless defined?(PLUGIN_ROOT)
+RAILS_ROOT = File.join(PLUGIN_ROOT, '..','..','..') unless defined?(RAILS_ROOT)
 
 # Workaround a problem with script/plugin and http-based repos.
 # See http://dev.rubyonrails.org/ticket/8189
